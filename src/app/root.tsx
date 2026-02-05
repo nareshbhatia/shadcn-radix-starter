9fc3b1c81e078152dd08082c3867fc345f6846df
Naresh Bhatia <me@nareshbhatia.dev>
2026-02-05 12:17:21 -0500

    allow main content area to scroll

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -2,11 +2,27 @@ import { Outlet } from 'react-router';
 
 import { TopBar } from '../components/TopBar/TopBar';
 
+/**
+ * Root layout component that wraps the entire application.
+ * - The application takes up the full height of the viewport.
+ * - The top bar is fixed height.
+ * - The main content area takes up the remaining space and is scrollable.
+ * - The routes are rendered in the main content area.
+ *
+ * |-------------------|
+ * |       TopBar      |
+ * |-------------------|
+ * | Main Content Area |
+ * |     <Outlet>      |
+ * |-------------------|
+ */
 export function RootLayout() {
   return (
-    <div className="flex flex-col min-h-screen relative">
+    <div className="flex flex-col h-screen relative">
       <TopBar />
-      <Outlet />
+      <main className="flex-1 min-h-0 overflow-x-hidden overflow-y-auto flex flex-col">
+        <Outlet />
+      </main>
     </div>
   );
 }
